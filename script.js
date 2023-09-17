6583e0c102419fc3d1b33695502f2c7b6f02a4a3
adityap5 <aditya29pippal@gmail.com>
2023-09-17 21:56:16 +0530

    1

---
+++ script.js
@@ -0,0 +1,21 @@
+var btn = document.querySelector("button");
+var h4 = document.querySelector("h4");
+var check =0;
+
+btn.addEventListener("click", function(){
+    if (check==0) {
+
+        btn.innerHTML="Remove"
+        btn.style.backgroundColor="red"
+        h4.innerHTML="Accepted!!";
+        h4.style.color="green";
+        check=1;
+    }else{
+        btn.innerHTML="Accept Request"
+        h4.innerHTML="Stranger";
+        h4.style.color="red";
+        check=0;
+    }
+    
+    
+})
